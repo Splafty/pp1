e94713c4791fbf671ef740028c33ae5bac14d9ed
s-013-22 <s-013-22@gumiforest.ae.krakow.pl>
2023-11-15 16:28:42 +0100

    Zadania z lekcji

---
+++ 06-Arrays/Exercise17.py
@@ -0,0 +1,12 @@
+# 17.	An array contains values: [[0,0,0],[0,0,0],[0,0,0]]. 
+# Create a program that replaces the values of the main diagonal with 1. 
+# Use proper loop statements. 
+# Display the modified array. 
+# Sample result:
+# 1 0 0
+# 0 1 0
+# 0 0 1
+
+arr = [[[0,0,0],
+        [0,0,0],
+        [0,0,0]]]
