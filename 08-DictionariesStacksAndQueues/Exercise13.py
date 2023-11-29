f34c2b044942b0078f787fbd851195f7ff3579f8
s-013-21 <s-013-21@gumiforest.ae.krakow.pl>
2023-11-29 16:22:44 +0100

    Lekcja

---
+++ 08-DictionariesStacksAndQueues/Exercise13.py
@@ -0,0 +1,2 @@
+# 13.	A program contains any defined dictionary, with any number of key-value pairs of information.
+# Write a program that displays the number of pairs of information available in the dictionary.
